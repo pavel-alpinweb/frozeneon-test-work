45863202a31b739d72e4280753fcc787c9a7f687
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-06-18 13:17:44 +0600

    create date filter

--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 import App from "./App.vue";
 import store from "./store";
 
+function dateFilter(value, type) {
+  const date = new Date(value);
+  if (type === "date") {
+    return date.toLocaleDateString();
+  } else if (type === "time") {
+    return date.toLocaleTimeString();
+  } else {
+    return new Date(value).toLocaleString();
+  }
+}
+
+Vue.filter("date", dateFilter);
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
