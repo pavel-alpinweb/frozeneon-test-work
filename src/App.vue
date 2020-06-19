ca1fdba871e8e19a2af46f77eea7cafb4fb1baf0
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-06-19 15:44:26 +0600

    display package info in modal window

--- src/App.vue
+++ src/App.vue
@@ -23,45 +23,41 @@
                 bordered
                 striped)
                   template(v-slot:cell(show_details)="row")
-                    b-button(size="sm" v-b-modal.modal-1) Details
+                    b-button(size="sm" v-b-modal.my-modal @click="showDetails(row)") Details
                   template(v-slot:cell(package.date)="data")
-                    b.text-info {{ data.value | date("date") }} 
+                    b.text-info {{ data.value | date("date") }}
             b-pagination(
               v-model="currentPage"
               :total-rows="rows"
               :per-page="perPage"
               aria-controls="results-table")
             
-            b-modal(id="modal-1" title="Details" size="xl" hide-footer)
+            b-modal(id="my-modal" title="Details" size="xl" hide-footer, v-if="currentPackage.name")
               b-jumbotron.packet-window
                 template(v-slot:header) 
-                  |Name - sass
-                  b-badge.packet-window__version 1.26.8
-                template(v-slot:lead)
-                  |description - A pure JavaScript implementation of Sass.
+                  |{{ currentPackage.name }}
+                  b-badge.packet-window__version {{ currentPackage.version }}
+                template(v-slot:lead) {{ currentPackage.description }}
                 b-avatar.mr-3
-                b-link(href="#") Natalie Weizenbaum
+                b-link(:href="currentPackage.author.url" target="blank") {{ currentPackage.author.name }}
                 hr.my-4
                 div.keywords
                   h4 
                     |Last published: 
-                    b-badge 05.06.2020
+                    b-badge {{ currentPackage.date | date("date") }}
                 hr.my-4
                 div.keywords
                   h4 Keywords:
-                  b-badge.keywords__badge(variant="success") style
-                  b-badge.keywords__badge(variant="success") scss
-                  b-badge.keywords__badge(variant="success") sass
-                  b-badge.keywords__badge(variant="success") preprocessor
-                  b-badge.keywords__badge(variant="success") css
+                  b-badge.keywords__badge(variant="success" v-for="item in currentPackage.keywords") {{ item }}
                 hr.my-4
                 div.links
                   h4 Links:
                   b-button-group
-                    b-button.links__item(href="#"  variant="danger") npm
-                    b-button.links__item(href="#"  variant="warning") homepage
-                    b-button.links__item(href="#"  variant="info") repository
-                    b-button.links__item(href="#"  variant="dark") bugs
+                    b-button.links__item(:href="currentPackage.links.npm" target="blank"  variant="danger") npm
+                    b-button.links__item(:href="currentPackage.links.homepage" target="blank"  variant="warning") homepage
+                    b-button.links__item(:href="currentPackage.links.repository" target="blank"  variant="info") repository
+                    b-button.links__item(:href="currentPackage.links.bugs" target="blank"  variant="dark") bugs
+
     footer.page-footer
       .container
         .row
@@ -76,6 +72,7 @@ export default {
       searchString: "",
       perPage: 10,
       currentPage: 1,
+      currentPackage: {},
       fields: [
         {
           key: "package.name",
@@ -114,6 +111,9 @@ export default {
       if (this.searchString !== "") {
         this.$store.dispatch("getAllPackages", this.searchString);
       }
+    },
+    showDetails(row) {
+      this.currentPackage = row.item.package;
     }
   }
 };
