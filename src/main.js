787fd76ff8c1cb6e5c8320dc674fb6f70e6e6748
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2020-06-20 15:11:32 +0600

    three shaking

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import Vue from "vue";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import { LayoutPlugin } from "bootstrap-vue";
+import { ModalPlugin } from "bootstrap-vue";
+import { TablePlugin } from "bootstrap-vue";
+import { FormInputPlugin } from "bootstrap-vue";
+import { FormDatepickerPlugin } from "bootstrap-vue";
+import { AlertPlugin } from "bootstrap-vue";
+import { SpinnerPlugin } from "bootstrap-vue";
+import { ButtonPlugin } from "bootstrap-vue";
+import { ButtonGroupPlugin } from "bootstrap-vue";
+import { PaginationPlugin } from "bootstrap-vue";
+import { ImagePlugin } from "bootstrap-vue";
+import { LinkPlugin } from "bootstrap-vue";
+import { JumbotronPlugin } from "bootstrap-vue";
+import { BadgePlugin } from "bootstrap-vue";
+import { AvatarPlugin } from "bootstrap-vue";
+import { ToastPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import App from "./App.vue";
@@ -18,8 +33,22 @@ function dateFilter(value, type) {
 
 Vue.filter("date", dateFilter);
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ModalPlugin);
+Vue.use(TablePlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormDatepickerPlugin);
+Vue.use(AlertPlugin);
+Vue.use(SpinnerPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(ButtonGroupPlugin);
+Vue.use(PaginationPlugin);
+Vue.use(ImagePlugin);
+Vue.use(LinkPlugin);
+Vue.use(JumbotronPlugin);
+Vue.use(BadgePlugin);
+Vue.use(AvatarPlugin);
+Vue.use(ToastPlugin);
 export const eventBus = new Vue();
 Vue.prototype.$eventBus = eventBus; // Global event bus
 
